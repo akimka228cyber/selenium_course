dae9a85ac5865805e91bef02f8045304ec20856f
akimka228cyber <akimhtcnew@gmail.com>
2025-12-10 21:48:02 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,8 +1,12 @@
+import time
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+# В линке убрал домен языка, чтобы корректно подтягивался язык браузера
+link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    time.sleep(30)
+    basket_button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    assert basket_button.is_displayed(), "Кнопка корзины не отображается на странице"
