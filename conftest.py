ad5e96e796be2f6b1789f77e1bbad151a81a2152
akimka228cyber <akimhtcnew@gmail.com>
2025-12-10 21:47:48 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,23 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose browser language")
+    parser.addoption('--language', action='store', default=None, help="Choose browser language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
 
+    options = webdriver.ChromeOptions()
 
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # У себя я использую Chrome for testing, но для проверки закомментирую реализацию через него
+    # options.binary_location = r"D:\Chrome-for-Test-win64\chrome.exe"
 
-    #options = webdriver.ChromeOptions()
-    #options.add_argument(f'--lang={language}')
-    
-    options.binary_location = r"D:\Chrome-for-Test-win64\chrome.exe"
+    # Реализовал установку языка браузера через аргумент lang
+    options.add_argument(f'--lang={language}')
+
 
     browser = webdriver.Chrome(service=Service(), options=options)
 
